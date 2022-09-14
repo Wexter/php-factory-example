1e0c52a54de78e1916c0f12634edcd54c2958391
Vitaliy <wesallenalex@gmail.com>
2022-09-15 00:38:48 +0300

    update test.php

--- test.php
+++ test.php
@@ -2,16 +2,53 @@
 
     require_once './vendor/autoload.php';
 
-    var_dump(\Lib\Media\Image\Storage\ImageStorageFactory::getRegisteredProviders());
+    use Lib\Media\Image\Storage\ImageStorage;
 
-    $imageStorage = \Lib\Media\Image\Storage\ImageStorageFactory::getStorage(\Lib\Media\Image\Storage\Type::DATABASE);
+    $providers = [
+        'Database',
+        'S3',
+        'Directory',
+        'NotFound',
+        'NotImplemented'
+    ];
 
-    // var_dump(get_class($imageStorage));
+    foreach ($providers as $provider) {
+        echo "Test {$provider} provider" . PHP_EOL;
+        try {
+            $imageStorage = ImageStorage::getStorage($provider); // OK
+    
+            echo "Provider \"{$provider}\" OK" . PHP_EOL;
+        } catch (Exception $e) { echo "Exception: {$e->getMessage()}" . PHP_EOL; }
+        echo PHP_EOL;
+    }
 
-    // $imageStorage = \Lib\Media\Image\Storage\ImageStorageFactory::getStorage(\Lib\Media\Image\Storage\Type::DIRECTORY);
+    echo "Test providers returned by ImageStorage::getRegisteredProviders()" . PHP_EOL . PHP_EOL;
 
-    // var_dump(get_class($imageStorage));
+    $providers = ImageStorage::getRegisteredProviders();
 
-    // $imageStorage = \Lib\Media\Image\Storage\ImageStorageFactory::getStorage(\Lib\Media\Image\Storage\Type::S3);
+    var_dump($providers);
 
-    // var_dump(get_class($imageStorage));
+    foreach ($providers as $provider) {
+        echo "Test {$provider} provider" . PHP_EOL;
+        try {
+            $imageStorage = ImageStorage::getStorage($provider); // OK
+   
+            var_dump(get_class($imageStorage));
+
+            echo "Provider \"{$provider}\" OK" . PHP_EOL;
+        } catch (Exception $e) { echo "Exception: {$e->getMessage()}" . PHP_EOL; }
+        echo PHP_EOL;
+    }
+    // var_dump($providers);
+
+    // foreach ($providers as $provider) {
+    //     echo "Requesting storage provider: {$provider}" . PHP_EOL;
+
+    //     $imageStorage = ImageStorage::getStorage($provider);
+
+    //     var_dump(get_class($imageStorage));
+    // }
+
+    // $imageStorage = new \Lib\Media\Image\Storage\Providers\S3ImageStorage();
+
+    // var_dump(get_declared_classes());
