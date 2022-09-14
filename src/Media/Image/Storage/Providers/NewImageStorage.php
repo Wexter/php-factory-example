5c4801d65ed8b30859c29485faef294014751158
Vitaliy <wesallenalex@gmail.com>
2022-09-15 00:37:19 +0300

    Remove NewImageStorage

--- src/Media/Image/Storage/Providers/NewImageStorage.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    namespace Lib\Media\Image\Storage\Providers;
-    
-    use Lib\Media\Image\Storage\ImageStorageInterface;
-
-    class NewImageStorage implements ImageStorageInterface{
-
-    }
