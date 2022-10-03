a27d2c26c995659795e830160c4d80bdef09756a
Vitaliy <wesallenalex@gmail.com>
2022-10-03 22:11:22 +0300

    Add provider load timer

--- test.php
+++ test.php
@@ -2,15 +2,24 @@
 
     require_once './vendor/autoload.php';
 
+    use Lib\Timer;
     use Lib\Storage\FileStorage;
     use Lib\Storage\ProviderInterface;
 
+    $timer = new Timer();
+
     $providers = FileStorage::getProviders();
+
+    echo "Providers load time {$timer->elapsed()}s" . PHP_EOL;
 
     var_dump($providers);
 
     foreach ($providers as $providerName) {
+        $timer->start();
+
         $fileStorage = new FileStorage($providerName);
+
+        echo "Provider create time: {$timer->elapsed()}s" . PHP_EOL;
 
         $provider = $fileStorage->getProvider();
 
